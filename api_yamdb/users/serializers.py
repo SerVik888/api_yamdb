673355597e74907b90ab7ea1850aab482ceadf66
Сафонов Сергей <sergey_safonov86@inbox.ru>
2023-10-03 18:05:10 +0300

    Finished working with users and registration.

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,36 +1,36 @@
 import random
-from django.core.validators import RegexValidator
 import re
-from rest_framework import status
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-from rest_framework.relations import SlugRelatedField
 from django.core.mail import send_mail
-from django.db import IntegrityError
-from django.shortcuts import get_object_or_404
 from django.conf import settings
-from rest_framework.response import Response
 
-from users.models import CustomUser, User
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+def validate_username(data):
+    """Если кто-то пытается создать пользователя с именем 'me'
+    или имя не соответствует требованиям отправляем ошибку."""
+    username = data.get('username')
+    if username:
+        if username == 'me' or not re.match(r'^[\w.@+-]+\Z', username):
+            raise ValidationError(
+                'Вы не можете зарегестрировать пользователя с таким именем.'
+            )
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = (
             'username', 'email', 'first_name', 'last_name', 'bio', 'role'
         )
 
     def validate(self, data):
-        # ^[\w.@+-]+\Z
-        """Если кто-то пытается создать пользователя с именем 'me',
-        отправляем ошибку"""
-        username = data.get('username')
-        # me = re.match(r'^[\w.@+-]+\Z',
-        #               'username'
-        #    data.get('username')
-        #   )
+        """Ещё проверяем что бы пользователь не мог поменять поле 'role'
+        при запросе к 'me'"""
         endpoint = self.context.get(
             'request'
         ).parser_context.get(
@@ -38,94 +38,41 @@ class CustomUserSerializer(serializers.ModelSerializer):
         ).get('username')
         if data.get('role') and endpoint == 'me':
             raise ValidationError(
-                'Вы не можете менять роль пользоваетля при запросе к этому эндпоитну'
+                'Вы не можете менять роль пользоваетля при запросе к '
+                'этому эндпоитну'
             )
-        if username:
-            if username == 'me' or not re.match(r'^[\w.@+-]+\Z', username):
-                raise ValidationError(
-                    'Вы не можете зарегестрировать пользователя с таким именем.'
-                )
-
-            # if not re.match(r'^[\w.@+-]+\Z', username):
-            #     raise ValidationError(
-            #         'Вы не можете.'
-            #     )
+        validate_username(data)
         return data
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ('email', 'username')
 
     def create(self, validated_data):
-
+        """При создании пользователя создаём код с которым пользователь может
+        войти в свою учётную записть и изменить данные.
+        Отправляем этот код на указанную почту."""
         code = str(random.randint(111111, 999999))
-        # code = validated_data.get('code')
-
         send_mail(
             subject='Подтверждение почты',
             message=f'Ваш код подтверждения: {code}',
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[validated_data.get('email')]
         )
-        validated_data['code'] = code
-        # user, created = User.objects.get_or_create(
-        #     username=validated_data.get('username'),
-        #     email=validated_data.get('email')
-        # )
-        try:
-            user, created = CustomUser.objects.get_or_create(
-                username=validated_data.get('username'),
-                email=validated_data.get('email')
-            )
-        except IntegrityError:
-            # self.cleaned_data
-            return Response(
-                {"field_name": []},
-                status=status.HTTP_400_BAD_REQUEST)
+        user, created = User.objects.get_or_create(
+            username=validated_data.get('username'),
+            email=validated_data.get('email')
+        )
 
         user.confirmation_code = code
         return user
 
-    # def update(self, instance, validated_data):
-    #     instance.save()
-    #     return instance
-
     def validate(self, data):
-        """Если кто-то пытается создать пользователя с именем 'me',
-        отправляем ошибку"""
-        username = data.get('username')
-        if username:
-            if username == 'me' or not re.match(r'^[\w.@+-]+\Z', username):
-                raise ValidationError(
-                    'Вы не можете зарегестрировать пользователя с таким именем.'
-                )
+        validate_username(data)
         return data
-
-
-# class UserProfileSerializer(serializers.ModelSerializer):
-#     role = SlugRelatedField(
-#         slug_field='role', read_only=True
-#     )
-
-#     class Meta:
-#         model = CustomUser
-#         fields = (
-#             'username', 'email', 'first_name', 'last_name', 'bio', 'role'
-#         )
-
-#     def validate(self, data):
-#         # ^[\w.@+-]+\Z
-#         """Если кто-то пытается создать пользователя с именем 'me',
-#         отправляем ошибку"""
-#         me = re.match('me')
-#         if data.get('username') == 'me':
-#             raise ValidationError(
-#                 'Вы не можете зарегестрировать пользователя с именем `me`.'
-#             )
-#         return data
 
 
 class ConfirmationCodeSerializer(serializers.Serializer):
