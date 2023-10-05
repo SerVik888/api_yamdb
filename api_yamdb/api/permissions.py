1fd76996f2c741e1a35e3a2ff751096f62b6d486
Oleg Korolev <mr.olkor@gmail.com>
2023-10-05 18:22:36 +0300

    Fixed all the feedback from the initial review.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,5 +1,4 @@
 from rest_framework import permissions
-from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
@@ -24,15 +23,7 @@ class IsAdminOrReadOnly(BasePermission):
 class AdminModeratorOwnerOrReadOnly(permissions.BasePermission):
     """Права доступа администратора, модератора или автора."""
 
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
     def has_object_permission(self, request, view, obj):
-        if request.method == 'PUT':
-            raise MethodNotAllowed(request.method)
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
